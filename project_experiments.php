cb3de35adb46463bf42f4655ec56a194666dd036
vijai <vijairajan@gmail.com>
2014-11-15 15:12:06 +0530

    initial project version

---
+++ project_experiments.php
@@ -0,0 +1,67 @@
+
+
+<html>
+  <head>
+    <title>i-SECURIT Project Experiments</title>
+    <style type="text/css">
+	    body{
+	      background:black;
+	      color:#80c0c0;
+	    }
+
+      #container{
+
+        margin-top: 10%;
+        margin-left: 25%;
+        margin-right: 25%;
+        color:white;
+        border: 1px solid red;
+        text-align: center;
+
+      }
+
+      a{
+     
+        text-decoration: none;
+    
+      }
+
+  	</style>
+
+  </head>
+  <body>
+
+	<div id="container">
+     	<br>
+     	<p>
+     		<h2>Click the link</h2>
+     		<h3>SHA1 and the Uploaded Project Experiments</h3>
+     	</p>
+     	
+      <br><p></p>	
+      
+      <p>
+        <h3>
+          SHA1
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <a href="../PHP/manage.php?SHA1_experiment=true" target="_blank"><input type="button" name="SHA1" id="SHA1" value="Click Here"></a>
+     	  </h3>
+      </p>
+      
+      <br>
+      
+      <p>
+        <h3>
+          UPLOADED PROJECTS
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <a href="../PHP/manage.php?project_experiments=true" target="_blank"><input type="button" name="project_experiments" id="project_experiments" value= "Click Here"> </a>
+        </h3>
+      </p>
+
+      <br>
+    
+    </div>
+
+  </body>
+
